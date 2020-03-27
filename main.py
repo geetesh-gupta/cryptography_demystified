254e99f3bd4ba0ecb581cbf70dcf15f803f3eaa5
Geetesh Gupta <gupta.15@iitj.ac.in>
2020-03-27 19:50:49 +0530

    Refactor main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import sys
+from sys import argv
 import des
 import aes
 import vernam
@@ -21,23 +21,34 @@ def run(msg, key, algo):
 
 
 def main():
-    if sys.argv[1] == "des":
-        run("0000000100100011010001010110011110001001101010111100110111101111",
-            "0001001100110100010101110111100110011011101111001101111111110001",
-            des)
-    elif sys.argv[1] == "aes":
-        run("0123456789abcdeffedcba9876543210",
-            "0f1571c947d9e8590cb7add6af7f6798",
-            aes)
-    elif sys.argv[1] == "vernam":
-        run("RAMSWARUPK",
-            "RANCHOBABA",
-            vernam)
-
-    elif sys.argv[1] == "caesar":
-        run("ABCDEFGHIJKLMNOPQRSTUVWXYZ",
-            23,
-            caesar)
+    if len(argv) < 2:
+        print("Usage: python3 main.py [algo] [msg] [key]")
+    elif len(argv) == 2:
+        if argv[1] == "des":
+            run("0000000100100011010001010110011110001001101010111100110111101111",
+                "0001001100110100010101110111100110011011101111001101111111110001",
+                des)
+        elif argv[1] == "aes":
+            run("0123456789abcdeffedcba9876543210",
+                "0f1571c947d9e8590cb7add6af7f6798",
+                aes)
+        elif argv[1] == "vernam":
+            run("RAMSWARUPK",
+                "RANCHOBABA",
+                vernam)
+        elif argv[1] == "caesar":
+            run("ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+                23,
+                caesar)
+    else:
+        if argv[1] == "des":
+            run(argv[2], argv[3], des)
+        elif argv[1] == "aes":
+            run(argv[2], argv[3], aes)
+        elif argv[1] == "vernam":
+            run(argv[2], argv[3], vernam)
+        elif argv[1] == "caesar":
+            run(argv[2], argv[3], caesar)
 
 
 if __name__ == "__main__":
