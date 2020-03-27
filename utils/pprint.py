c48dc9e2cb1296894f7a6bdafb589e4293ef6e38
Geetesh Gupta <gupta.15@iitj.ac.in>
2020-03-27 18:47:33 +0530

    Update pprint.py

--- utils/pprint.py
+++ utils/pprint.py
@@ -6,3 +6,13 @@ def pprint(verbosity, *args):
         for arg in args:
             print(arg, end=" ")
         print()
+
+
+def hexprint(arg):
+    if isinstance(arg, list):
+        lis = []
+        for i in arg:
+            lis.append(hexprint(i))
+        return lis
+    else:
+        return hex(arg)
