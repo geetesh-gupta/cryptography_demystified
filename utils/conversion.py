c60b2845bea8fe7edb434c7974231cbba0a658c6
Geetesh Gupta <gupta.15@iitj.ac.in>
2020-03-27 19:32:56 +0530

    Remove redundant code

--- utils/conversion.py
+++ utils/conversion.py
@@ -109,24 +109,3 @@ def inc_bytes(a):
             out[i] += 1
             break
     return bytes(out)
-
-
-def pad(plaintext):
-    """
-    Pads the given plaintext with padding to a multiple of 16 bytes.
-    """
-    padding_len = 16 - (len(plaintext) % 16)
-    padding = bytes([padding_len] * padding_len)
-    return plaintext + padding
-
-
-def unpad(plaintext):
-    """
-    Removes a padding, returning the unpadded text and ensuring the
-    padding was correct.
-    """
-    padding_len = plaintext[-1]
-    assert padding_len > 0
-    message, padding = plaintext[:-padding_len], plaintext[-padding_len:]
-    assert all(p == padding_len for p in padding)
-    return message
