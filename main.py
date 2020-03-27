a17e0b97b945640a2950b4f8cf6d652dba616cd5
Geetesh Gupta <gupta.15@iitj.ac.in>
2020-03-27 19:33:47 +0530

    Refactor main.py

--- main.py
+++ main.py
@@ -4,34 +4,30 @@ import aes
 from utils.pprint import pprint
 
 
+def run(msg, key, algo):
+    pprint(0, "Msg", msg)
+    pprint(0, "Key", key)
+
+    ciphertext = algo.encrypt(msg, key)
+    pprint(0, "Cipher", ciphertext)
+
+    plaintext = algo.decrypt(ciphertext, key)
+    pprint(0, "PlainText", plaintext)
+
+    if plaintext == msg:
+        pprint(0, "Plaintext is equal to Msg")
+
+
 def main():
     if sys.argv[1] == "des":
-        msg = "0000000100100011010001010110011110001001101010111100110111101111"
-        key = "0001001100110100010101110111100110011011101111001101111111110001"
-        pprint(0, "Msg", msg)
-        pprint(0, "Key", key)
-
-        ciphertext = des.encrypt(msg, key)
-        pprint(0, "Cipher", ciphertext)
-
-        plaintext = des.decrypt(ciphertext, key)
-        pprint(0, "PlainText", plaintext)
-
-        if plaintext == msg:
-            pprint(0, "Plaintext is equal to Msg")
+        run("0000000100100011010001010110011110001001101010111100110111101111",
+            "0001001100110100010101110111100110011011101111001101111111110001",
+            des)
     elif sys.argv[1] == "aes":
-        msg = "0123456789abcdeffedcba9876543210"
-        key = "0f1571c947d9e8590cb7add6af7f6798"
-        pprint(0, "Msg", msg)
-        pprint(0, "Key", key)
+        run("0123456789abcdeffedcba9876543210",
+            "0f1571c947d9e8590cb7add6af7f6798",
+            aes)
 
-        ciphertext = aes.encrypt(msg, key)
-        pprint(0, "Cipher", ciphertext)
 
-        plaintext = aes.decrypt(ciphertext, key)
-        pprint(0, "PlainText", plaintext)
-
-        if plaintext == msg:
-            pprint(0, "Plaintext is equal to Msg")
 if __name__ == "__main__":
     main()
