ab63dd2935dd794f1bc0d53038331076ba8626e9
Geetesh Gupta <gupta.15@iitj.ac.in>
2020-03-27 18:48:45 +0530

    Add aes to main

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import sys
-from des import encrypt, decrypt
+import des
+import aes
 from utils.pprint import pprint
 
 
@@ -10,14 +11,23 @@ def main():
         pprint(0, "Msg", msg)
         pprint(0, "Key", key)
 
-        ciphertext = encrypt(msg, key)
+        ciphertext = des.encrypt(msg, key)
         pprint(0, "Cipher", ciphertext)
 
-        plaintext = decrypt(ciphertext, key)
+        plaintext = des.decrypt(ciphertext, key)
+        des.encrypt(ciphertext, key)
         pprint(0, "PlainText", plaintext)
 
         if plaintext == msg:
             pprint(0, "Plaintext is equal to Msg")
+    elif sys.argv[1] == "aes":
+        msg = "0123456789abcdeffedcba9876543210"
+        key = "0f1571c947d9e8590cb7add6af7f6798"
+        pprint(0, "Msg", msg)
+        pprint(0, "Key", key)
+
+        ciphertext = aes.encrypt(msg, key)
+        pprint(0, "Cipher", ciphertext)
 
 
 if __name__ == "__main__":
