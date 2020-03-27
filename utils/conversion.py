db305d41a883aaa8fa8cc07ff75992999bd9ee13
Geetesh Gupta <gupta.15@iitj.ac.in>
2020-03-27 18:47:42 +0530

    Update conversion.py

--- utils/conversion.py
+++ utils/conversion.py
@@ -30,3 +30,51 @@ def hexbin(hex_str):
 
 def binhex(bin_str):
     return convert_to_hex(bin_str, 2)
+
+
+def permute(old_bits, offset_table, offset):
+    new_bits = ""
+    for i in range(len(offset_table)):
+        new_bits += old_bits[offset_table[i] - offset]
+    return new_bits
+
+
+def pad(string, padding=8):
+    to_pad = len(string) % padding
+    str_padded = string[to_pad:]
+    if to_pad:
+        str_padded = string[:to_pad].zfill(padding) + str_padded
+    return str_padded
+
+
+def lcs(string, shift):
+    return string[shift:] + string[:shift]
+
+
+def rcs(string, shift):
+    return lcs(string, len(string) - shift)
+
+
+def xorstr(str1, str2):
+    xored_str = ""
+    for i in range(len(str1)):
+        if str1[i] == str2[i]:
+            xored_str += "0"
+        else:
+            xored_str += "1"
+    return xored_str
+
+
+def hexstr2hexlist(hexstr, offset):
+    lis = []
+    for i in range(0, len(hexstr), offset):
+        lis.append(int(hexstr[i:i + offset], 16))
+    return lis
+
+
+def hexlist2hexstr(hexlist, offset):
+    string = ""
+    for i in range(len(hexlist)):
+        for j in range(len(hexlist[i])):
+            string += convert_to_hex(hexlist[i][j], 10)
+    return string
